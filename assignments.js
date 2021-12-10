dba286b88fe340efd385b93204d9c4f2b60d9e49
andrius-siup <andrius.siupinys@gmail.com>
2021-12-10 12:45:57 +0000

    Challenge - 2

--- assignments.js
+++ assignments.js
@@ -56,8 +56,31 @@ description = `Portugal is in Europe, and its 11 million people speak portuguese
 
 console.log(description);
 
+/**
+  JavaScript Fundamentals – Part 1 
+ 
+  Coding Challenge #1 
+  Mark and John are trying to compare their BMI (Body Mass Index), which is 
+  calculated using the formula: 
+  BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+  and height in meter). 
+  Your tasks: 
+  1. Store Mark's and John's mass and height in variables 
+  2. Calculate both their BMIs using the formula (you can even implement both 
+  versions) 
+  3. Create a Boolean variable 'markHigherBMI' containing information about 
+  whether Mark has a higher BMI than John. 
+  Test data: 
+  § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+  m tall. 
+  § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+  m tall. 
+ */
+
 /*
 // CHALLENGE PART-1
+
+
 console.log("CHALLENGE PART-1 - DATA -1");
 
 let weightMark = 78;
@@ -100,3 +123,44 @@ console.log("Mark BMI - Data 2 " + ": " + bmiMarkD2);
 console.log("John BMI - Data 2 " + ": " + bmiJohnD2);
 console.log("Mark has higher BMI ? " + markHigherBMId2);
 */
+
+/**
+Coding Challenge #2 
+Use the BMI example from Challenge #1, and the code you already wrote, and 
+improve it. 
+Your tasks: 
+1. Print a nice output to the console, saying who has the higher BMI. The message 
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!" 
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's 
+BMI (28.3) is higher than John's (23.9)!"
+ */
+
+console.log("CHALLENGE PART-1 - CHALLENGE - 2");
+
+let weightMark = 78;
+let heightMark = 1.69;
+let weightJohn = 92;
+let heightJohn = 1.95;
+
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+
+// Both formula is correct
+// bmiMark = weightMark / heightMark ** 2;
+bmiMark = weightMark / (heightMark * heightMark);
+
+bmiJohn = weightJohn / (heightJohn * heightJohn);
+
+let markHigherBMI = bmiMark > bmiJohn;
+
+console.log("Mark BMI" + ": " + bmiMark);
+console.log("John BMI" + ": " + bmiJohn);
+console.log("Mark has higher BMI ? " + markHigherBMI);
+let message;
+
+if (bmiMark > bmiJohn) {
+  message = `Mark's BMI ${bmiMark} is higher than John's ${bmiJohn}!`;
+} else {
+  message = `John's BMI is higher than Mark's!`;
+}
+
+console.log(message);
