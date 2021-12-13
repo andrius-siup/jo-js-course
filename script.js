78817a99e8f58640191ae91c8602ede9a1af7ca6
andrius-siup <andrius.siupinys@gmail.com>
2021-12-13 10:52:26 +0000

    Functions

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 "use strict"; // activating strict mode show error
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive!");
+
+// reserved, strict not allowed
+// const interface = "Audio";
+// const private = 534;
+// const if = false;
+*/
+
+// -------------------------- Functions
+function logger() {
+  console.log("My name is Andrius");
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  //console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
