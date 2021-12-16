d002b8b8aa28938336a28fdb58539299c15dfe56
andrius-siup <andrius.siupinys@gmail.com>
2021-12-16 13:12:03 +0000

    Loop in another loop

--- script.js
+++ script.js
@@ -424,3 +424,12 @@ const andreArray = [
 for (let i = andreArray.length - 1; i >= 0; i--) {
   console.log(i, andreArray[i]);
 }
+
+// Loop in another loop
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------ Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
