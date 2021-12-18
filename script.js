03bc9aaf4dc2f7f63858eece7a38d9d7a934d6c0
andrius-siup <andrius.siupinys@gmail.com>
2021-12-18 14:43:57 +0000

    Got a problem

--- script.js
+++ script.js
@@ -470,3 +470,12 @@ while (dice !== 6) {
   if (dice === 6) console.log("Loop is about to end...");
 }
 */
+// Problem:
+/*
+We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temp amplitude. Keep in mind that sometimes there might be a sensor error."
+*/
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+
+// 2) Breaking up into sub-problems
