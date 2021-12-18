334746cbb2538dcfb368c0f1a259844b90b16e6d
andrius-siup <andrius.siupinys@gmail.com>
2021-12-18 15:34:57 +0000

    Fouded error word, and call temperatures into function and found amplitude(max - min)

--- script.js
+++ script.js
@@ -490,12 +490,18 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
-  for (let i = 0; i < temperatures.length; i++) {
+
+  for (let i = 0; i < temps.length; i++) {
     const currTemp = temps[i];
+    // fouded error
+    if (typeof currTemp !== "number") continue;
+
     if (currTemp > max) max = currTemp;
     if (currTemp < min) min = currTemp;
   }
   console.log(max, min);
+  return max - min;
 };
 
-calcTempAmplitude([3, 6, 9, 45]);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
