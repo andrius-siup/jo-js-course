39ee71faf22880c5fb90bec317057d12f4c65cb3
andrius-siup <andrius.siupinys@gmail.com>
2021-12-20 21:12:44 +0000

    Challenge - 1 - added three dots

--- assignments.js
+++ assignments.js
@@ -1066,8 +1066,8 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
   let str = ""; // empty string
   for (let i = 0; i < arr.length; i++) {
-    str = str + `${arr[i]}°C in ${i + 1} days `;
+    str = str + `${arr[i]}°C in ${i + 1} days ...`;
   }
-  console.log(str);
+  console.log("..." + str);
 };
 printForecast(data1);
