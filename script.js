4832715e5624785e5ffcac9d8a69901f7206d97c
andrius-siup <andrius.siupinys@gmail.com>
2021-12-20 19:44:53 +0000

    Debugging: created function with bug in measureKelvin.value, by default promt returns string

--- script.js
+++ script.js
@@ -487,6 +487,7 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
+/*
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
@@ -505,7 +506,7 @@ const calcTempAmplitude = function (temps) {
 
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
-
+*/
 // Problem 2:
 // Function should now receive 2 arrays of temps
 
@@ -514,7 +515,7 @@ console.log(amplitude);
 
 // 2) Breaking up into sub-problems
 // - Merge two arrays?
-
+/*
 const calcTempAmplitudeNew = function (t1, t2) {
   // concatinate two arrays
   const temps = t1.concat(t2);
@@ -537,3 +538,20 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: prompt("Degrees celsius"),
+  };
+
+  console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// a) Identify bug
+console.log(measureKelvin());
